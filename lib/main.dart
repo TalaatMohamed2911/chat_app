ea02ec5f27b528d59a0fd12f8acb7c5ac6f6d217
TalaatMohamed2911 <talaatmohamed2001@gmail.com>
2025-04-07 07:08:10 +0200

    last version

--- lib/main.dart
+++ lib/main.dart
@@ -18,8 +18,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
-  // welcome
-  // Talaat
+  // Hello Friends :)
   @override
   Widget build(BuildContext context) {
     return MaterialApp(

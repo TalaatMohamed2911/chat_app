fd3c033161d9baa37b33ac92bac08b66d4cf7b5b
TalaatMohamed2911 <talaatmohamed2001@gmail.com>
2025-04-07 06:06:59 +0200

    branch2 addition

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
 
   // This widget is the root of your application.
   // welcome
+  // Talaat
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
